dee724293ded1524538e324a38a38721552aa765
DHGiang <dhgiang1@cmc.com.vn>
2018-08-31 20:38:36 +0700

    Base Source Code

---
+++ src/main/java/com/example/Group5/Group5Application.java
@@ -0,0 +1,12 @@
+package com.example.Group5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5Application.class, args);
+	}
+}
