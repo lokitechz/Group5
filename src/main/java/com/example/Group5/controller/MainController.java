3a1f7cdbb8abfc35734155228b8b3184ef553799
Dương Hoàng Giang <vuachom94@gmail.com>
2018-09-26 11:30:05 +0700

    Thêm giao diện trang thống kê

--- src/main/java/com/example/Group5/controller/MainController.java
+++ src/main/java/com/example/Group5/controller/MainController.java
@@ -1,11 +1,31 @@
 package com.example.Group5.controller;
 
+import com.example.Group5.entity.AppUser;
+import com.example.Group5.entity.UserRole;
+import com.example.Group5.repository.AppRoleRepo;
+import com.example.Group5.repository.AppUserRepo;
+import com.example.Group5.repository.RoleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
+
+    @Autowired
+    RoleRepo roleRepo;
+
+    @Autowired
+    AppUserRepo appUserRepo;
+
+    @Autowired
+    AppRoleRepo appRoleRepo;
+
     // Trang main khi chương trình chạy lên
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcomePage() {
@@ -24,4 +44,17 @@ public class MainController {
         return "Common/403Page";
     }
 
+    //  Trả về trang thống kê số liệu
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String dashboard(Model model) {
+//        long roleId = 2;
+//        List<UserRole> userRoles = roleRepo.findAllByAppRole(appRoleRepo.findById(roleId));
+//        List<AppUser> users = new ArrayList<>();
+//        for (UserRole userRole : userRoles) {
+//            users.add(userRole.getAppUser());
+//        }
+//
+//        model.addAttribute("countUser", users);
+        return "Common/Dashboard";
+    }
 }
