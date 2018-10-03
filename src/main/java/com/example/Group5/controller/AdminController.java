7bb7c63f24600a38d0757bf2dcf8107dddb69813
Hoàng Xuân Vũ <vuhx@vnext.com.vn>
2018-10-03 17:38:22 +0700

    Thêm phần sửa trạng thái của vé nhưng chưa xong

--- src/main/java/com/example/Group5/controller/AdminController.java
+++ src/main/java/com/example/Group5/controller/AdminController.java
@@ -7,12 +7,12 @@ import com.example.Group5.repository.TicketRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -23,6 +23,7 @@ public class AdminController {
     @Autowired
     private TicketRepo ticketRepo;
 
+    // Tìm tất danh sách những khách hàng đã đặt vé xe theo mã xe
     @RequestMapping(value = "/manage-bus/details/{id}", method = RequestMethod.GET)
     public String listTicket(@PathVariable int id, Model model) {
         List<Ticket> tickets = ticketRepo.findAllByBusId(id);
@@ -35,8 +36,31 @@ public class AdminController {
                     model.addAttribute("CustomerInfo", customers);
                 }
             }
-            model.addAttribute("TicketInfo",ticket);
+            model.addAttribute("TicketInfo", ticket);
         }
         return "ManageTicket/ListTicket";
     }
+
+    //  Trả về trang sửa trạng thái của vé
+    @RequestMapping(path = "/manage-bus/updateTicket/{id}", method = RequestMethod.GET)
+    public String editTicketPage(@PathVariable int id, Model model) {
+        Optional<Ticket> ticket = ticketRepo.findById(id);
+        if (ticket.isPresent()) {
+            model.addAttribute("detailTicket", ticket.get());
+        }
+        return "ManageTicket/UpdateTicket";
+    }
+
+    // Lưu trạng thái của vé lên DB
+    @RequestMapping(value = "/manage-bus/updateTicket/{id}", method = RequestMethod.POST)
+    public String updateTicket(Model model, @PathVariable int id, @ModelAttribute Ticket ticket, RedirectAttributes red) {
+        Ticket item = ticketRepo.findById(id).get();
+        if (!ticket.isStatus()){
+            red.addFlashAttribute("msg","Cập nhật thành công");
+            return "redirect:/manage-bus";
+        }
+        red.addFlashAttribute("msg","Cập nhật thành công");
+        return "redirect:/manage-bus";
+    }
+
 }
