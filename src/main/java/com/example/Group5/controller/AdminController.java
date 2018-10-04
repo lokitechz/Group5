44981e90908c8097fbf4dd79923630bc2411946d
DHGiang1 <vuachom94@gmai.com>
2018-10-04 21:13:58 +0700

    Thêm trang thống kê số liệu

--- src/main/java/com/example/Group5/controller/AdminController.java
+++ src/main/java/com/example/Group5/controller/AdminController.java
@@ -1,26 +1,18 @@
-//package com.example.Group5.controller;
-//
-//import com.example.Group5.entity.AppUser;
-//import com.example.Group5.repository.AppUserRepo;
-//import com.example.Group5.repository.TicketRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Controller
-//public class AdminController {
-//
-//    @Autowired
-//    private AppUserRepo appUserRepo;
-//
-//    @Autowired
-//    private TicketRepo ticketRepo;
+package com.example.Group5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AdminController {
+
+    //  Trả về trang thống kê số liệu
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String Dashboard() {
+        return "Common/Dashboard";
+    }
+}
 //
 //    // Tìm tất danh sách những khách hàng đã đặt vé xe theo mã xe
 //    @RequestMapping(value = "/manage-bus/details/{id}", method = RequestMethod.GET)
